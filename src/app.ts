c566c5e6687a9402ae68e4eedf7a0e8f00cff90f
Juan Formoso <trajano.juan@gmail.com>
2022-02-23 22:13:07 -0300

    Add userRouter and errorMiddleware at app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import usersRouter from './routes/users';
+import error from './middlewares/error';
 
 const app = express();
-
 app.use(express.json());
+
+app.use('/users', usersRouter);
+app.use(error);
 
 export default app;
