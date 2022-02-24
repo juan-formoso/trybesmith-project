963ad743b0bcc7b90d83f51f2677b1a52cff5e44
Juan Formoso <trajano.juan@gmail.com>
2022-02-24 16:11:59 -0300

    Retira next() de error middleware

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import Joi from 'joi';
 
-const error = (err: unknown, req: Request, res: Response, next: NextFunction) => {
+const error = (err: unknown, req: Request, res: Response, _next: NextFunction) => {
   if (Joi.isError(err)) {
     if (err?.details[0].type === 'any.required') {
       return res.status(400).json({
@@ -10,7 +10,6 @@ const error = (err: unknown, req: Request, res: Response, next: NextFunction) =>
     }
     return res.status(422).json({ error: err.message });
   }
-  next();
 };
 
 export default error;
