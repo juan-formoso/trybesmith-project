c6ebec1d2e58d1ab56b59c10374d3f77f4318ad5
Juan Formoso <trajano.juan@gmail.com>
2022-02-23 22:00:34 -0300

    Add error middleware

---
+++ src/middlewares/error.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from 'express';
+import Joi from 'joi';
+
+const error = (err: unknown, req: Request, res: Response, next: NextFunction) => {
+  if (Joi.isError(err)) {
+    if (err?.details[0].type === 'any.required') {
+      return res.status(400).json({
+        error: err.message,
+      });
+    }
+    return res.status(422).json({ error: err.message });
+  }
+  next();
+};
+
+export default error;
