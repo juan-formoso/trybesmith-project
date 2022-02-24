2e8f4d42b87d69064a5b7dfb4bd64f13381a3c80
Juan Formoso <trajano.juan@gmail.com>
2022-02-23 22:07:39 -0300

    Add userServices

---
+++ src/services/users.ts
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken';
+import User from '../interfaces/User';
+import Token from '../interfaces/Token';
+import usersModel from '../models/users';
+
+const createUser = async (user: User): Promise<Token> => {
+  await usersModel.createUser(user);
+  const secret = process.env.JWT_SECRET || 'any_secret';
+  const token = jwt.sign({ userId: user.id }, secret);
+  return { token };
+};
+
+export default { createUser };
