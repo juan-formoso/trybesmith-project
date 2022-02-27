c9dc8394e41673a504732c7a1a5d63efadc61b0a
Juan Formoso <trajano.juan@gmail.com>
2022-02-26 22:27:19 -0300

    Update usersController

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -3,11 +3,11 @@ import usersService from '../services/users';
 import User from '../interfaces/User';
 import Token from '../interfaces/Token';
 import validationJoi from '../middlewares/joiValidation';
-import userSchema from '../middlewares/schemas';
+import Schemas from '../middlewares/schemas';
 import 'express-async-errors';
 
 const createUser = async (req: Request, res: Response): Promise<Response> => {
-  validationJoi(userSchema, req.body);
+  validationJoi(Schemas.userSchema, req.body);
   const user: User = req.body;
   const token: Token = await usersService.createUser(user);
   return res.status(201).json(token);
