7e576e243bb403d3fa5e6167dc74902ee54bda09
Juan Formoso <trajano.juan@gmail.com>
2022-02-26 22:29:58 -0300

    Update index.ts with loginRouter

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import usersRouter from './users.routes';
+import loginRouter from './login.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
+routes.use('/login', loginRouter);
 
 export default routes;
