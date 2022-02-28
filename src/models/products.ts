213aedb7ba305ae287adf02a5395851d6f92739b
Juan Formoso <trajano.juan@gmail.com>
2022-02-28 19:31:57 -0300

    Add product Model

---
+++ src/models/products.ts
@@ -0,0 +1,14 @@
+import { ResultSetHeader } from 'mysql2';
+import { Product } from '../interfaces/Product';
+import connection from './connection';
+
+const createProduct = async (product: Product): Promise<number> => {
+  const [rows] = await connection.execute<ResultSetHeader>(
+    'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?)',
+    [product.name, product.amount],
+  );
+  const { insertId } = rows;
+  return insertId;
+};
+
+export default createProduct;
